4cd4d85c9393448e6302505aa4eeddc413ec4f31
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-06-04 11:38:56 +0530

    feat: modified user menu based on current user

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,11 +1,16 @@
 'use client';
 
+import { SafeUser } from '@/app/types';
 import Container from '../Container';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: SafeUser | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -15,7 +20,7 @@ const Navbar = () => {
 
             <Search />
 
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
