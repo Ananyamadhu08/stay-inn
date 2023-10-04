dff5fd87c78545510ea601cf72bcac1d33d37872
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-10-04 23:31:22 +0530

    style: changed label styles

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -55,9 +55,9 @@ const Search = () => {
       className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
     >
       <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">{locationLabel}</div>
+        <div className="text-sm text-gray-600 px-6">{locationLabel}</div>
 
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+        <div className="hidden sm:block text-sm text-gray-600 px-6 border-x-[1px] flex-1 text-center">
           {durationLabel}
         </div>
 
